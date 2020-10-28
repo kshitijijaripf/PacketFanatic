f7cff49a2fbb06ab3d69d6c45fd3e731359a0d7e
kshitijijari <ijarikshitij@gmail.com>
2020-10-28 22:29:55 +0530

    Second commit

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import  './Home.css'
 
 function Home() {
     return (
+      <div className="home">
         <nav className="navbar">
       <img 
        className="navbar__logo" 
@@ -10,7 +11,27 @@ function Home() {
        alt=""
       />
         </nav>
+         <div>
+         <img 
+        className="home__image"
+        src="
+        https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.YtZHNxAb2_dJIBQGYjCCAQHaCp%26pid%3DApi&f=1"          
+        alt=""
+         />
+         </div >
+         <div className ="Table__row">
+          <div>
+           <h1>Hello</h1>
+          </div>
+         <div>
+           <h1>hiiii</h1>
+         </div>
+         <div>
+           <h1>bye</h1>
+         </div>
 
+       </div>
+       </div>
        
     )
 }
